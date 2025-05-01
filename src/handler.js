70ba8c4deb20bdb174d27ce0d26168af070eafbb
Nolan McCafferty <nolanm@omnistrate.com>
2025-05-01 07:33:25 -0700

    update handler to v2
    Signed-off-by: Nolan McCafferty <nolanm@omnistrate.com>

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello world v1!',
+        message: 'Hello world v2!',
         input: event,
       },
       null,
